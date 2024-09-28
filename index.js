6476f5f2b0e15ee4ee4a3f01ed65ac4a53debb04
jonah <jonahmcooper@gmail.com>
2024-09-27 20:49:33 -0700

    begin game

---
+++ index.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor(position){
+        this.position = position
+    }
+     
+    draw(){
+        c.fillStyle = 'green'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
